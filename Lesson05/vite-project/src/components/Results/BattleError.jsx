237b5818d19de10135db7102dbafd7c486fc8ea0
Eduard <eddnovik@gmail.com>
2023-05-02 18:42:58 +0200

    add lesson07

---
+++ Lesson05/vite-project/src/components/Results/BattleError.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom'
+
+
+const BattleError = ({err}) => {
+    return (
+        <div className="column-error">
+            <h3>{err}</h3>
+            <h3>Please choose other players</h3>
+            <Link className='button' to='/battle'>Go to Battle</Link>
+        </div>
+    );
+};
+
+export default BattleError;
--- Lesson05/vite-project/src/components/Results/Results.jsx
+++ Lesson05/vite-project/src/components/Results/Results.jsx
@@ -1,20 +1,71 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { battle } from "../api";
+import  PlayerPreview  from '../Battle/PlayerPreview'
+import BattleInfo from "./BattleInfo";
+import Loader from '../Popular/Loader'
+import BattleError from "./BattleError";
+
 
 const Results = () => {
-    const location = useLocation()
-    // const location = window.location
-    
+  const location = useLocation();
+  const [battleResultData, setBattleResultData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [err, setErr] = useState()
 
-    useEffect(() => {
-        const params = new URLSearchParams(location.search)
-        console.log(params.get('playerOneName'), params.get('playerTwoName'), 'location');
 
-    }, [])
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    console.log(params.get("playerOneName"),params.get("playerTwoName"),"location");
 
-    return (
-        <h2>Results</h2>
-    );
+    const getBattleResults = async () => {
+      setIsLoading(true)
+      try {
+        const players = await battle([
+          params.get("playerOneName"),
+          params.get("playerTwoName"),
+        ])
+        setBattleResultData(players);
+        
+      } catch (error) {
+        setErr(error.message);
+        console.log(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getBattleResults();
+  }, []);
+
+  return (
+    <div>
+      {err ? (
+        <BattleError err={err} />
+      ) : isLoading ? (
+        <Loader />
+      ) : (
+        <div className="row">
+          {battleResultData.map((player, index) => {
+            return (
+              <PlayerPreview
+                key={index}
+                avatar={player.profile.avatar_url}
+                username={player.profile.login}
+              >
+                <BattleInfo player={player} index={index} />
+              </PlayerPreview>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Results;
+
+
+
+
+
+
