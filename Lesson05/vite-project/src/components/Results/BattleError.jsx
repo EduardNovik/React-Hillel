8eeff310ccf42a660b93dd4c7ab10e9dc807a078
Eduard <108239776+EduardNovik@users.noreply.github.com>
2023-05-08 23:34:41 +0200

    Update BattleError.jsx

--- Lesson05/vite-project/src/components/Results/BattleError.jsx
+++ Lesson05/vite-project/src/components/Results/BattleError.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 
-const BattleError = ({err}) => {
-    return (
-        <div className="column-error">
-            <h3>{err}</h3>
-            <h3>Please choose other players</h3>
-            <Link className='button' to='/battle'>Go to Battle</Link>
-        </div>
-    );
+const BattleError = () => {
+  const err = useSelector((state) => state.results.error);
+
+  return (
+    <div className="column-error">
+      <h3>{err}</h3>
+      <h3>Please choose other players</h3>
+      <Link className="button" to="/battle">
+        Go to Battle
+      </Link>
+    </div>
+  );
 };
 
 export default BattleError;
